dd2c86cbda7b22fb2fc1a91dd5f3eaacda4e4f56
mihir <mihirgala33@gmail.com>
2025-02-19 14:34:48 +0530

    Bug fix

--- app/(normal)/legal/tos/page.tsx
+++ app/(normal)/legal/tos/page.tsx
@@ -67,7 +67,7 @@ const TOSpage = () => {
   );
 };
 
-export const Section = ({ title, children }:{title:string,children:React.ReactNode}) => (
+const Section = ({ title, children }:{title:string,children:React.ReactNode}) => (
   <div>
     <h2 className="text-xl font-semibold mb-2">{title}</h2>
     {children}
