aa7591da16c3e99bf6c09c78c7f200c0343d8437
mihir <mihirgala33@gmail.com>
2025-02-27 20:50:35 +0530

    Model change

--- lib/google-generative-ai.ts
+++ lib/google-generative-ai.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
 export const JSONmodel = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-lite-preview-02-05",
+    model: "gemini-2.0-flash-lite",
     // Set the `responseMimeType` to output JSON
     generationConfig: { responseMimeType: "application/json" }
   })
